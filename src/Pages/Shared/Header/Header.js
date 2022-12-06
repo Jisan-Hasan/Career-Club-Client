bca104188cc6a3d7c2bfc025ca98083f889dfaa5
Jisan-Hasan <jisan.jh5@gmail.com>
2022-12-07 00:06:05 +0600

    logout functionality added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,44 +1,56 @@
-import { Button, Navbar } from 'flowbite-react';
-import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthProvider';
+import { Button, Navbar } from "flowbite-react";
+import React, { useContext } from "react";
+import { toast } from "react-hot-toast";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthProvider";
 
 const Header = () => {
-    const {user} = useContext(AuthContext);
+    const { user, logout } = useContext(AuthContext);
+    const handleLogout = () => {
+        logout()
+            .then(() => {})
+            .catch((err) => {
+                toast.error(`err.message`);
+            });
+    };
     return (
         <>
             <Navbar className="my-4 shadow-sm" fluid={true} rounded={true}>
-            <Link to="/">
-                <span className="self-center text-emerald-600 whitespace-nowrap text-4xl font-bold dark:text-white">
-                    CareerClub
-                </span>
-            </Link>
-            <div className="flex md:order-2">
-                {user ? (
-                    <Button color="failure" pill={true}>
-                        Sign Out
-                    </Button>
-                ) : (
-                    <Link to="/login">
-                        <Button color="success" pill={true}>
-                            Login
+                <Link to="/">
+                    <span className="self-center text-emerald-600 whitespace-nowrap text-4xl font-bold dark:text-white">
+                        CareerClub
+                    </span>
+                </Link>
+                <div className="flex md:order-2">
+                    {user ? (
+                        <Button
+                            onClick={handleLogout}
+                            color="failure"
+                            pill={true}
+                        >
+                            Sign Out
                         </Button>
+                    ) : (
+                        <Link to="/login">
+                            <Button color="success" pill={true}>
+                                Login
+                            </Button>
+                        </Link>
+                    )}
+                    <Navbar.Toggle />
+                </div>
+                <Navbar.Collapse>
+                    <Link to="/" className="hover:text-blue-600">
+                        Home
                     </Link>
-                )}
-                <Navbar.Toggle />
-            </div>
-            <Navbar.Collapse>
-                <Link to='/' className="hover:text-blue-600">
-                    Home
-                </Link>
-                <Link className="hover:text-blue-600" to="/services">
-                    Services
-                </Link>
-                <Link to='/blogs' className="hover:text-blue-600">
-                    Blogs
-                </Link>
-            </Navbar.Collapse>
-        </Navbar>
+                    <Link className="hover:text-blue-600" to="/services">
+                        Services
+                    </Link>
+                    <Link to="/blogs" className="hover:text-blue-600">
+                        Blogs
+                    </Link>
+                </Navbar.Collapse>
+            </Navbar>
         </>
     );
 };
