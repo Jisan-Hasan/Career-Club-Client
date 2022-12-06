54552d412ecce1d62de24d37b88c1c2ddf9f591b
Jisan-Hasan <jisan.jh5@gmail.com>
2022-12-07 00:38:56 +0600

    signin with email password feature added

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,13 @@
 import { Button, Label, TextInput } from "flowbite-react";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { toast } from "react-hot-toast";
 import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { saveUser, setUserRole, setUserVerifyStatus } from "../../api/auth";
 import { AuthContext } from "../../contexts/AuthProvider";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
     const {
         user,
         loading,
@@ -20,7 +21,29 @@ const Login = () => {
     } = useContext(AuthContext);
 
     const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
 
+    // implement email password signin
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        // get form data
+        const password = form.password.value;
+
+        signIn(email, password)
+            .then((result) => {
+                // const user = result.user;
+                // show success
+                toast.success("Signin Successfully.");
+                navigate(from);
+            })
+            .catch((err) => {
+                toast.error(`${err.message}`);
+            });
+    };
+
+    // implement google signin
     const handleGoogleSignIn = () => {
         signInWithGoogle()
             .then((result) => {
@@ -34,7 +57,7 @@ const Login = () => {
                 setUserVerifyStatus(user?.email, false);
                 // show success
                 toast.success("Signin Successfully.");
-                navigate("/");
+                navigate(from);
             })
             .catch((err) => {
                 toast.error(`err.message`);
@@ -43,14 +66,16 @@ const Login = () => {
     return (
         <div className="max-w-lg mx-auto mt-10  md:mt-14 lg:mt-20 bg-gray-100 px-12 py-14 rounded-xl divide-y-2">
             <h3 className="text-3xl font-bold mb-5 text-center">Login</h3>
-            <form className="flex flex-col">
+            <form onSubmit={handleSubmit} className="flex flex-col">
                 <div className="mt-4">
                     <div className="mb-2 block">
                         <Label htmlFor="email" value="Your email" />
                     </div>
                     <TextInput
+                        onBlur={(e) => setEmail(e.target.value)}
                         id="email"
                         type="email"
+                        name="email"
                         placeholder="jisan@gmail.com"
                         required={true}
                     />
@@ -62,6 +87,7 @@ const Login = () => {
                     <TextInput
                         id="password"
                         type="password"
+                        name="password"
                         placeholder="******"
                         required={true}
                     />
@@ -79,7 +105,7 @@ const Login = () => {
                 <div className="mt-2 mb-3">
                     <div className="mb-2 text-sm block">
                         Don't have any account?{" "}
-                        <Link to='/signup' className="text-sm text-blue-500">
+                        <Link to="/signup" className="text-sm text-blue-500">
                             Sign Up
                         </Link>
                     </div>
